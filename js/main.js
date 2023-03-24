f7acfd0f425e4636a376d1b9768d0adaa186ae33
Hyojin <dlgywls1130@gmail.net>
2023-03-25 02:15:54 +0900

    main slide

--- js/main.js
+++ js/main.js
@@ -3,11 +3,11 @@ $(window).on('load', function () {
 })
 
 function setFlowBanner() {
-    const $wrap = $('.track');
-    const $list = $('.track .motion_wrap');
+    const $wrap = $('.flow_banner');
+    const $list = $('.flow_banner .list');
     let wrapWidth = ''; //$wrap의 가로 크기
     let listWidth = ''; //$list의 가로 크기
-    const speed = 60; //1초에 몇픽셀 이동하는지 설정
+    const speed = 92; //1초에 몇픽셀 이동하는지 설정
 
     //리스트 복제
     let $clone = $list.clone();
@@ -28,8 +28,8 @@ function setFlowBanner() {
     function flowBannerAct() {
         //배너 롤링 초기화
         if (wrapWidth != '') {
-            $wrap.find('.motion_wrap').css({ 'animation': 'none' });
-            $wrap.find('.motion_wrap').slice(2).remove();
+            $wrap.find('.list').css({ 'animation': 'none' });
+            $wrap.find('.list').slice(2).remove();
         }
         wrapWidth = $wrap.width();
         listWidth = $list.width();
@@ -42,6 +42,57 @@ function setFlowBanner() {
                 $wrap.append($clone);
             }
         }
-        $wrap.find('.motion_wrap').css({ 'animation': `${listWidth / speed}s linear infinite flowRolling` });
+        $wrap.find('.list').css({ 'animation': `${listWidth / speed}s linear infinite flowRolling` });
+    }
+}
+
+
+
+
+$(window).on('load', function () {
+    setFlowBanner1();
+})
+
+function setFlowBanner1() {
+    const $wrap1 = $('.flow_banner1');
+    const $list1 = $('.flow_banner1 .list1');
+    let wrapWidth1 = ''; //$wrap의 가로 크기
+    let listWidth1 = ''; //$list의 가로 크기
+    const speed1 = 60; //1초에 몇픽셀 이동하는지 설정
+
+    //리스트 복제
+    let $clone1 = $list1.clone();
+    $wrap1.append($clone1);
+    flowBannerAct1()
+
+    //반응형 :: 디바이스가 변경 될 때마다 배너 롤링 초기화
+    let oldWChk = window.innerWidth > 1279 ? 'pc' : window.innerWidth > 767 ? 'ta' : 'mo';
+    $(window).on('resize', function () {
+        let newWChk = window.innerWidth > 1279 ? 'pc' : window.innerWidth > 767 ? 'ta' : 'mo';
+        if (newWChk != oldWChk) {
+            oldWChk = newWChk;
+            flowBannerAct1();
+        }
+    });
+
+    //배너 실행 함수
+    function flowBannerAct1() {
+        //배너 롤링 초기화
+        if (wrapWidth1 != '') {
+            $wrap1.find('.list1').css({ 'animation': 'none' });
+            $wrap1.find('.list1').slice(2).remove();
+        }
+        wrapWidth1 = $wrap1.width();
+        listWidth1 = $list1.width();
+
+        //무한 반복을 위해 리스트를 복제 후 배너에 추가
+        if (listWidth1 < wrapWidth1) {
+            const listCount1 = Math.ceil(wrapWidth1 * 2 / listWidth1);
+            for (let i = 2; i < listCount1; i++) {
+                $clone1 = $clone1.clone();
+                $wrap1.append($clone1);
+            }
+        }
+        $wrap1.find('.list1').css({ 'animation': `${listWidth1 / speed1}s linear infinite flowRolling` });
     }
 }
