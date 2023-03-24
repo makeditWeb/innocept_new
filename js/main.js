4ab9e367a9b4f4d8c3290d4cd9f0d59da3c7d644
picknumber-hyojin <hyojin_lee11@picknumber.co.kr>
2023-03-24 15:32:49 +0900

    작업중

--- js/main.js
+++ js/main.js
@@ -1,27 +1,47 @@
+$(window).on('load', function () {
+    setFlowBanner();
+})
 
+function setFlowBanner() {
+    const $wrap = $('.track');
+    const $list = $('.track .motion_wrap');
+    let wrapWidth = ''; //$wrap의 가로 크기
+    let listWidth = ''; //$list의 가로 크기
+    const speed = 60; //1초에 몇픽셀 이동하는지 설정
 
+    //리스트 복제
+    let $clone = $list.clone();
+    $wrap.append($clone);
+    flowBannerAct()
 
-// slide
-function marquee(selector, speed) {
-    const parentSelector = document.querySelector(selector);
-    const clone = parentSelector.innerHTML;
-    const firstElement = parentSelector.children[0];
-    let i = 0;
-  
-    parentSelector.insertAdjacentHTML("beforeend", clone);
-    parentSelector.insertAdjacentHTML("beforeend", clone);
-  
-    setInterval(function () {
-      firstElement.style.marginLeft = `-${i}px`;
-      if (i > firstElement.clientWidth) {
-        i = 0;
-      }
-      i = i + speed;
-    }, 0);
-  }
-  
-  //after window is completed load
-  //1 class selector for marquee
-  //2 marquee speed 0.2
-  window.addEventListener("load", marquee(".marquee", 0.2));
-  
+    //반응형 :: 디바이스가 변경 될 때마다 배너 롤링 초기화
+    let oldWChk = window.innerWidth > 1279 ? 'pc' : window.innerWidth > 767 ? 'ta' : 'mo';
+    $(window).on('resize', function () {
+        let newWChk = window.innerWidth > 1279 ? 'pc' : window.innerWidth > 767 ? 'ta' : 'mo';
+        if (newWChk != oldWChk) {
+            oldWChk = newWChk;
+            flowBannerAct();
+        }
+    });
+
+    //배너 실행 함수
+    function flowBannerAct() {
+        //배너 롤링 초기화
+        if (wrapWidth != '') {
+            $wrap.find('.motion_wrap').css({ 'animation': 'none' });
+            $wrap.find('.motion_wrap').slice(2).remove();
+        }
+        wrapWidth = $wrap.width();
+        listWidth = $list.width();
+
+        //무한 반복을 위해 리스트를 복제 후 배너에 추가
+        if (listWidth < wrapWidth) {
+            const listCount = Math.ceil(wrapWidth * 2 / listWidth);
+            for (let i = 2; i < listCount; i++) {
+                $clone = $clone.clone();
+                $wrap.append($clone);
+            }
+        }
+        $wrap.find('.motion_wrap').css({ 'animation': `${listWidth / speed}s linear infinite flowRolling` });
+    }
+}
