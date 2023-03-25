315eed7d7aede1d47ef43d7ba79520d5862a8656
Hyojin <dlgywls1130@gmail.net>
2023-03-26 00:22:51 +0900

    main page animation완료

--- js/main.js
+++ js/main.js
@@ -1,28 +1,9 @@
-const sections = document.querySelectorAll('section');
-const sectionsArray = Array.from(sections);
-
-// 첫 번째 섹션을 화면에 보이도록 스크롤 이동
-sectionsArray[0].scrollIntoView();
-
-window.addEventListener('scroll', () => {
-    const currentPosition = window.pageYOffset;
-
-    // 현재 위치에서 다음 섹션의 위치까지의 거리가 화면의 높이 이하일 때 다음 섹션으로 이동
-    for (let i = 0; i < sectionsArray.length - 1; i++) {
-        const section = sectionsArray[i];
-        const nextSection = sectionsArray[i + 1];
-        const sectionTop = section.offsetTop;
-        const nextSectionTop = nextSection.offsetTop;
-
-        if (currentPosition >= sectionTop && currentPosition < nextSectionTop) {
-            nextSection.scrollIntoView({ behavior: 'smooth' });
-            break;
-        }
-    }
+// fullpage js
+$(document).ready(function () {
+    $('#fullpage').fullpage({
+        scrollBar: true
+    });
 });
-
-
-
 
 // Handle scrolling when tapping on the navbar menu
 function scrollIntoView(selector) {
@@ -44,6 +25,30 @@ navbarMenu.addEventListener('click', (event) => {
     scrollIntoView(link);
 });
 
+
+
+// 스크롤시 navbar 색상 변경 및 logo 이미지 변경
+window.addEventListener('scroll', function () {
+    var navbar = document.querySelector('nav.navbar');
+    var sections = document.querySelectorAll('section');
+    var currentSectionIndex = 0;
+
+    for (var i = 0; i < sections.length; i++) {
+        if (sections[i].offsetTop - navbar.offsetHeight <= window.pageYOffset) {
+            currentSectionIndex = i;
+        }
+    }
+
+    var currentSection = sections[currentSectionIndex];
+
+    if (currentSection.classList.contains('bg-black')) {
+        navbar.classList.remove('bg-white');
+        // 로고 이미지를 바꾸는 코드 추가
+    } else {
+        navbar.classList.add('bg-white');
+        // 로고 이미지를 바꾸는 코드 추가
+    }
+});
 
 
 // text slide
@@ -96,4 +101,56 @@ function setFlowBanner() {
             $wrap.find('.list').css({ 'animation': `${listWidth / speed}s linear infinite flowRolling` });
         }
     });
+}
+
+
+// text right slide
+
+
+$(window).on('load', function () {
+    setFlowBanner2();
+})
+
+function setFlowBanner2() {
+    const $wrap2 = $('.flow_banner');
+    const $list2 = $('.flow_banner .motion_rigth');
+    let wrapWidth2 = ''; //$wrap의 가로 크기
+    let listWidth2 = ''; //$list의 가로 크기
+    const speed2 = 92; //1초에 몇픽셀 이동하는지 설정
+
+    //리스트 복제
+    let $clone2 = $list2.clone();
+    $wrap2.append($clone2);
+    flowBannerAct2()
+
+    //반응형 :: 디바이스가 변경 될 때마다 배너 롤링 초기화
+    let oldWChk = window.innerWidth > 1279 ? 'pc' : window.innerWidth > 767 ? 'ta' : 'mo';
+    $(window).on('resize', function () {
+        let newWChk = window.innerWidth > 1279 ? 'pc' : window.innerWidth > 767 ? 'ta' : 'mo';
+        if (newWChk != oldWChk) {
+            oldWChk = newWChk;
+            flowBannerAct2();
+        }
+    });
+
+    //배너 실행 함수
+    function flowBannerAct2() {
+        //배너 롤링 초기화
+        if (wrapWidth2 != '') {
+            $wrap2.find('.motion_rigth').css({ 'animation': 'none' });
+            $wrap2.find('.motion_rigth').slice(2).remove();
+        }
+        wrapWidth2 = $wrap2.width();
+        listWidth2 = $list2.width();
+
+        //무한 반복을 위해 리스트를 복제 후 배너에 추가
+        if (listWidth2 < wrapWidth2) {
+            const listCount2 = Math.ceil(wrapWidth2 * 2 / listWidth2);
+            for (let i = 2; i < listCount2; i++) {
+                $clone2 = $clone2.clone();
+                $wrap2.append($clone2);
+            }
+        }
+        $wrap2.find('.motion_rigth').css({ 'animation': `${listWidth2 / speed2}s linear infinite flowRolling1` });
+    }
 }
