64a5be8f144a1c5d939aaa054ee4291031c9641a
Hyojin <dlgywls1130@gmail.net>
2023-04-04 00:07:36 +0900

    수정중

--- js/main.js
+++ js/main.js
@@ -189,25 +189,4 @@ jQuery(function ($) {
 });
 
 
-$(document).ready(function () {
 
-    const tabItem = document.querySelectorAll(".tab-container__item");
-    const tabContent = document.querySelectorAll(".content-container__content");
-
-    tabItem.forEach((item) => {
-        item.addEventListener("click", tabHandler);
-    });
-
-    function tabHandler(item) {
-        const tabTarget = item.currentTarget;
-        const target = tabTarget.dataset.tab;
-        tabItem.forEach((title) => {
-            title.classList.remove("active");
-        });
-        tabContent.forEach((target) => {
-            target.classList.remove("target");
-        });
-        document.querySelector("#" + target).classList.add("target");
-        tabTarget.classList.add("active");
-    }
-})
